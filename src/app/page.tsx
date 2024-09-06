436c89ca11447b350772f3a9fee16c165804428d
Elizaveta139 <elizavetarevina139@gmail.com>
2024-09-06 12:01:52 +0300

    delete not active

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import ActiveLabel from './components/active-label';
-import NotActiveLabel from './components/not-active-label';
 
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Home page</h1>
+        <h1 className="text-xl">Home page</h1>
         <ActiveLabel>Active</ActiveLabel>
-        <NotActiveLabel>Not active</NotActiveLabel>
       </main>
     </div>
   );
