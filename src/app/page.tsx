3b20150b01433490971c6a11f81f2721e9773cdf
Elizaveta139 <elizavetarevina139@gmail.com>
2024-09-13 12:56:50 +0300

    ServerComponentCopy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import AddCompanyButton from './components/add-company-button';
+import ClientComponent from './components/client-component';
+import ServerComponent from './components/server-component';
+import ServerComponentCopy from './components/server-component copy';
 // import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
@@ -7,6 +10,10 @@ export default function Home() {
       <main>
         <h1 className="text-xl">Home page</h1>
         <AddCompanyButton />
+        <ServerComponent />
+        <ClientComponent>
+          <ServerComponentCopy />
+        </ClientComponent>
       </main>
     </div>
   );
