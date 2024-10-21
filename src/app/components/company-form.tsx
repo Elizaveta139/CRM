0dbba9a1aafe4686c9d09595c1ec5ada8adc6119
Elizaveta139 <elizavetarevina139@gmail.com>
2024-10-21 12:02:50 +0300

    Update company-form.tsx

--- src/app/components/company-form.tsx
+++ src/app/components/company-form.tsx
@@ -34,7 +34,7 @@ export default function CompanyForm({ onSubmit }: CompanyFormProps) {
       <Form className="flex flex-col gap-10">
         <p className="mb-0.5 text-xl">Add new company</p>
         <div className="flex gap-6">
-          {/* <div className="flex flex-col flex-1 gap-5">
+          <div className="flex flex-col flex-1 gap-5">
             <LogoUploader label="Logo" placeholder="Upload photo" />
             <InputField label="Status" placeholder="Status" name="status" />
             <InputField label="Country" placeholder="Country" name="country" />
@@ -44,7 +44,7 @@ export default function CompanyForm({ onSubmit }: CompanyFormProps) {
             <InputField label="Category" placeholder="Category" name="category" />
             <InputField label="Joined date" type="date" name="date" />
             <InputField label="Description" placeholder="Description" name="description" />
-          </div> */}
+          </div>
         </div>
         <Button type="submit">Add company</Button>
       </Form>
