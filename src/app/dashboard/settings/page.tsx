8356de3a41a7a1441889c332b483882d64df8cf6
Elizaveta139 <elizavetarevina139@gmail.com>
2024-10-28 17:44:46 +0200

    Create page.tsx

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+export interface DashboardPageProps {}
+
+export default function DashboardPage({}: DashboardPageProps) {
+  return (
+    <div>
+      <main>
+        <h1 className="text-xl">Settings page</h1>
+      </main>
+    </div>
+  );
+}
