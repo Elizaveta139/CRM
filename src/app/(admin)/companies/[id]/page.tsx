00baeb285e7b859b35a524d32900980633bc67a3
Elizaveta139 <elizavetarevina139@gmail.com>
2024-10-30 21:59:10 +0200

    додані хуки в сайдбар

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from '../../components/header';
+import Header from '@/app/components/header';
+// import Header from '../../components/header';
 
 // export interface IdCompaniesPageProps {
 //   params: { id: string[] };
--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,25 +1,25 @@
-// import Header from '@/app/components/header';
-// import Toolbar from '@/app/components/toolbar';
-// import SearchInput from '@/app/components/search-input';
-// import AddCompanyButton from '@/app/components/add-company-button';
-// import CompanyTable from '@/app/components/company-table';
-// import CompanyRow from '@/app/components/company-row';
-// import { Status } from '@/app/components/status-label';
+import Header from '@/app/components/header';
+import Toolbar from '@/app/components/toolbar';
+import SearchInput from '@/app/components/search-input';
+import AddCompanyButton from '@/app/components/add-company-button';
+import CompanyTable from '@/app/components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { Status } from '@/app/components/status-label';
 import React from 'react';
-import Headers from '../components/header';
-import Toolbar from '../components/toolbar';
-import SearchInput from '../components/search-input';
-import AddCompanyButton from '../components/add-company-button';
-import CompanyTable from '../components/company-table';
-import CompanyRow from '../components/company-row';
-import { Status } from '../components/status-label';
+// import Headers from '../components/header';
+// import Toolbar from '../components/toolbar';
+// import SearchInput from '../components/search-input';
+// import AddCompanyButton from '../components/add-company-button';
+// import CompanyTable from '../components/company-table';
+// import CompanyRow from '../components/company-row';
+// import { Status } from '../components/status-label';
 
 export interface CompaniesPageProps {}
 
 export default function CompaniesPage({}: CompaniesPageProps) {
   return (
     <>
-      <Headers>Companies</Headers>
+      <Header>Companies</Header>
       <Toolbar action={<AddCompanyButton />}>
         <SearchInput />
       </Toolbar>
