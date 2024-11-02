460817798a91a4b7374dfb9fdf995b4aa61eb68c
Elizaveta139 <elizavetarevina139@gmail.com>
2024-11-02 14:49:44 +0200

    update file

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,36 +1,15 @@
 'use client';
 
-import React, { useEffect } from 'react';
-import Header from '@/app/components/header';
-import { notFound } from 'next/navigation';
-// import Header from '../../components/header';
-
-// export interface IdCompaniesPageProps {
-//   params: { id: string[] };
-// }
-
-// export default function IdCompaniesPage({ params }: IdCompaniesPageProps) {
-//   return (
-//     <>
-//       <Headers>Companies ({String(params.id)})</Headers>
-//       <p>{new Date().toTimeString()} </p>
-//     </>
-//   );
-// }
+import React from 'react';
+// import Header from '@/app/components/header';
 
 export interface PageProps {
   params: { id: string };
 }
 export default function Page({ params }: PageProps) {
-  useEffect(() => {
-    const id = Number.parseInt(params.id);
-    if (Number.isNaN(id)) {
-      notFound();
-    }
-  }, [params.id]);
   return (
-    <>
-      <Header>Company ({params.id})</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,7 +1,3 @@
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import SearchInput from '@/app/components/search-input';
-import AddCompanyButton from '@/app/components/add-company-button';
 import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
@@ -19,10 +15,6 @@ export interface CompaniesPageProps {}
 export default function CompaniesPage({}: CompaniesPageProps) {
   return (
     <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
       <CompanyTable>
         <CompanyRow
           id={1}
@@ -32,7 +24,7 @@ export default function CompaniesPage({}: CompaniesPageProps) {
           promotion={true}
           country="USA"
           joinedDate="02.19.2023"
-        ></CompanyRow>
+        />
       </CompanyTable>
     </>
   );
