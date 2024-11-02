e0d521929b9d3410fbc8a4e3138e9006269bed4f
Elizaveta139 <elizavetarevina139@gmail.com>
2024-11-02 12:50:20 +0200

    api

---
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { getSummaryCategories } from '@/lib/api';
+import StatCard, { StatCardType } from '@/app/components/stat-card';
+import DashboardCard from '@/app/components/dashboard-card';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return <div>/* content */</div>;
+}
