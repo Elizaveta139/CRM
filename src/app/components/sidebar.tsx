59b701eae9e2bb797767edfb3503d5f44e6e2224
Elizaveta139 <elizavetarevina139@gmail.com>
2024-12-09 09:04:40 +0200

    Update sidebar.tsx

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -19,13 +19,13 @@ export default function SidebarProps({}: SidebarProps) {
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
-        <Image
+        {/* <Image
           className="py-8 mb-11 mx-auto"
           width={122}
           height={25}
           src="/icons/logo.svg"
           alt="logo"
-        />
+        /> */}
         <ul className="space-y-7">
           <SidebarItem
             current={pathname === '/dashboard'}
@@ -44,10 +44,10 @@ export default function SidebarProps({}: SidebarProps) {
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto" onClick={handleExitClick}>
+        {/* <button className="flex items-center gap-2 p-6 mt-auto mx-auto" onClick={handleExitClick}>
           <Image width={18} height={18} src="/icons/arrow-left-on-rectangle.svg" alt="logo" />
           <span className="font-medium text-white">Exit</span>
-        </button>
+        </button> */}
       </div>
     </aside>
   );
